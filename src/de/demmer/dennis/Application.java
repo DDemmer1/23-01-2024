16b48f366fd0a5e2db5bb2900de0b5b301d3bfac
Unknown <Unknown@DESKTOP-4KSK0J5.fritz.box>
2024-01-23 11:20:40 +0100

    initial commit

--- src/de/demmer/dennis/Application.java
+++ src/de/demmer/dennis/Application.java
@@ -1,9 +1,38 @@
 package de.demmer.dennis;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Application {
-	
+
 	public static void main(String[] args) {
 		
+		try {
+			FileInputStream fis = new FileInputStream("files/faust.txt");
+			InputStreamReader isr = new InputStreamReader(fis);
+			
+			int temp = isr.read();
+			
+			while(temp >= 0) {
+				System.out.print((char)temp);
+				temp = isr.read();
+			}
+			isr.close();
+			fis.close();
+			
+		} catch(FileNotFoundException fnf) {
+			fnf.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
 	}
+
+	
 
 }
